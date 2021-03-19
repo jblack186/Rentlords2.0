3960c184ee13b78e90e9686012c2ba6afb1d20bd
Jamison Blackwell <jamie@Jamisons-MacBook-Pro.local>
2021-03-19 13:30:47 -0400

    tenant dashboard is finished

--- rentlord/server/models/electricalSchema.js
+++ rentlord/server/models/electricalSchema.js
@@ -10,7 +10,7 @@ const electricalSchema = new Schema({
     recieved: {type: Boolean, default: false},
     completed: {type: Boolean, default: false},
     image: {type: String, default: ''},
-    date: {type: String, default: Date.now()}
+    date: {type: String, default:  new Date().toDateString()}
 
 })
 mongoose.model('electrical', electricalSchema);
