5743c78373f62a9a014a8a953a8e28da404363c2
Jamison Blackwell <jamie@Jamisons-MacBook-Pro.local>
2021-03-22 20:58:25 -0400

    created build folder for netligy

---
+++ rentlord/server/rlrds-client/build/precache-manifest.a96cf557b419c4d47491ce7e641755ce.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fd5ac86af72f9d7ebd6bcd9ab5af003",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3b7422618e51948a6ef",
+    "url": "/static/css/2.f4d56223.chunk.css"
+  },
+  {
+    "revision": "81aa0d294433269aa6b4",
+    "url": "/static/css/main.31e8ad0f.chunk.css"
+  },
+  {
+    "revision": "f3b7422618e51948a6ef",
+    "url": "/static/js/2.4892486f.chunk.js"
+  },
+  {
+    "revision": "7f9c2d220a8c14284cba5525e6f9a9c2",
+    "url": "/static/js/2.4892486f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81aa0d294433269aa6b4",
+    "url": "/static/js/main.3d63df43.chunk.js"
+  },
+  {
+    "revision": "8f6976fb772f52801b6e",
+    "url": "/static/js/runtime-main.2d077718.js"
+  },
+  {
+    "revision": "e06dd1aef54cfc2993fdd33f20a21ce8",
+    "url": "/static/media/fixing.e06dd1ae.png"
+  },
+  {
+    "revision": "628a7c912abac94d55849ad47027fd4e",
+    "url": "/static/media/handshake.628a7c91.svg"
+  },
+  {
+    "revision": "7cc9a6852fafdee88633d0c55227730f",
+    "url": "/static/media/profile-user.7cc9a685.svg"
+  }
+]);
